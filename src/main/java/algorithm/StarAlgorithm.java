62259bc7ca7e25fd3afad37d764f3dd167e4d936
Seetraum <834889737@qq.com>
2019-12-11 22:16:19 +0800

    add algorithm

---
+++ src/main/java/algorithm/StarAlgorithm.java
@@ -0,0 +1,20 @@
+package java.algorithm;
+/**
+ * A星寻路算法
+ * */
+public class StarAlgorithm {
+    //迷宫地图
+    public static final int[][] MAZE ={
+            {0,0,0,0,0,0,0},
+            {0,0,0,1,0,0,0},
+            {0,0,0,1,0,0,0},
+            {0,0,0,1,0,0,0},
+            {0,0,0,0,0,0,0}
+    };
+
+    /**
+     * A*寻路
+     * @param
+     * @param
+     * */
+}
